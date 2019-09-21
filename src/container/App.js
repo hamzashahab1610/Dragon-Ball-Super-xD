499f32cfb1873636168f02f80b7abc9259c188c1
hamzashahab1610 <hamzashahab1610@gmail.com>
2019-09-21 11:20:54 -0700

    Till connect

--- src/container/App.js
+++ src/container/App.js
@@ -5,31 +5,49 @@ import {warriors} from '../component/warriors.js';
 import Logo from '../1.png';
 import Scroll from '../component/scroll';
 import ErrorBoundry from '../component/ErrorBoundry';
+import { connect } from 'react-redux';
+
+import { setSearchField } from '../actions'
+
+const mapStateToProps = state => {
+    return {
+        searchField: state.searchField
+    }
+}
+
+const mapDispatchToProps = (dispatch) => {
+    return {
+        onSearchChange: (event) => dispatch(setSearchField(event.target.value))
+    }    
+}
 
 class App extends Component {
     constructor() {
         super();
         this.state = {
-            searchfield: '',
+            //searchfield: '',
             warriors: warriors
         }
     }
 
-    onSearchChange = (event) => {  
-        this.setState({searchfield: event.target.value});        
+    // onSearchChange = (event) => {  
+    //     this.setState({searchfield: event.target.value});        
         
-    }
+    // }
 
-    render(){
+    render() {
+        //console.log(this.props.store.getState())
+        const { searchField, onSearchChange } = this.props;
+        const { warriors } = this.state;
         const filter = this.state.warriors.filter(warriors => {
-            return warriors.name.toLowerCase().includes(this.state.searchfield.toLowerCase());
+            return warriors.name.toLowerCase().includes(searchField.toLowerCase());
         })
         return(
             <div className='tc'>
                 <div className='center grow'>
                     <img src={Logo} className='h-100 w-50' alt=''/>
                 </div> 
-                <div><SearchBox searchChange={this.onSearchChange}/></div> 
+                <div><SearchBox searchChange={onSearchChange}/></div> 
                 {/* <Scroll> */}
                     {/* <ErrorBoundry> */}
                         <div><br></br><CardList warriors={filter}/></div>
@@ -41,4 +59,4 @@ class App extends Component {
     
 }
 
-export default App;
+export default connect(mapStateToProps, mapDispatchToProps)(App); 
